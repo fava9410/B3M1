11d3cb062f1069ad0316dc3ddd23f78ec8904ad8
Alex <fava9410@hotmail.com>
2020-12-20 17:34:31 -0500

    Added more data and APIs changed

--- webapp/banco/banco/views.py
+++ webapp/banco/banco/views.py
@@ -3,31 +3,48 @@ from django.views.decorators.csrf import csrf_exempt
 import uuid
 import json
 
+
+def check_client(id_client, numero_tarjeta=None):
+    with open('data/clients.json', 'r') as f:
+        clients = json.load(f)
+    print(id_client)
+    for client in clients:
+        if client['id'] == id_client and numero_tarjeta is None:
+            return client
+        elif client['id'] == id_client and client['numero_tarjeta'] == numero_tarjeta:
+            return True
+    
+    return None
+
+
 @csrf_exempt
 def debitar(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
+            id_cliente = data['id_cliente']
             numero_tarjeta = data['numero_tarjeta']
-            destino = data['destino']
+            id_evento = data['id_evento']
             valor = data['valor']
         except:
             response = JsonResponse({'error': 'Parametros incorrectos'})
             response.status_code = 500
             return response
-        key = f'{numero_tarjeta}{destino}{valor}'
-        return JsonResponse({'id_transaccion': uuid.uuid5(uuid.NAMESPACE_DNS, key)})
+        
+        if check_client(id_cliente, numero_tarjeta):
+            key = f'{id_cliente}{numero_tarjeta}{id_evento}{valor}'
+            return JsonResponse({'id_transaccion': uuid.uuid5(uuid.NAMESPACE_DNS, key)})
+        else:
+            response = JsonResponse({'error': 'id_cliente y/o numero de tarjeta incorrectos'})
+            response.status_code = 500
+            return response
 
-    
+
 def cliente(request, id):    
     if request.method == 'GET':
-
-        with open('data/clients.json', 'r') as f:
-            clients = json.load(f)
-
-        for client in clients:
-            if client['id'] == id:
-                return JsonResponse(client)
+        client = check_client(id)
+        if client:
+            return JsonResponse(client)
         
         response = JsonResponse({'error': 'No existe el usuario'})
         response.status_code = 404
--- webapp/recibo/recibo/views.py
+++ webapp/recibo/recibo/views.py
@@ -4,12 +4,13 @@ import base64
 import json
 
 @csrf_exempt
-def obtener_qr(request):
+def new_qr(request):
     if request.method == "POST":
         try:
             data = json.loads(request.body)
-            id_usuario = data['id_usuario']
+            id_cliente = data['id_cliente']
             id_evento = data['id_evento']
+            id_transaccion = data['id_transaccion']
         except:
             response = JsonResponse({'error': 'parametros incorrectos'})
             response.status_code = 500
@@ -18,19 +19,4 @@ def obtener_qr(request):
         with open("recibos/qr/qr.png", "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read())
 
-        with open('data/qrs.json') as qrs_file:
-            qrs = json.load(qrs_file)
-        
-        for qr in qrs:
-            if qr['id_usuario'] == id_usuario:
-                if qr['id_evento'] == id_evento:
-                    return HttpResponse(encoded_string)
-
-        response = JsonResponse({'error': 'No se encontro usuario y/o evento'})
-        response.status_code = 404
-        return response        
-    elif request.method == 'GET':
-        with open('data/qrs.json') as qrs_file:
-            qrs = json.load(qrs_file)
-        
-        return JsonResponse(qrs, safe=False)
+        return HttpResponse(encoded_string)
